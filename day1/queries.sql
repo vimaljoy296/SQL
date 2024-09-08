5db3cbd5422112f61048717ee7899ee9d2ef4f25
Varsha-ACAD <varsha.v@atriauniversity.edu.in>
2024-09-08 07:18:28 +0000

    Pending changes exported from your codespace

--- day1/queries.sql
+++
@@ -1,5 +0,0 @@
--- For each of the following questions, write an SQL query to retrieve the required data.
-
--- Question 1: List all the books available.
-
--- Question 2: For each book available, list the title and author.
---
+++ day5/songify/queries.sql
@@ -0,0 +1,33 @@
+-- queries.sql
+-- Answer the following 10 questions by writing SQL SELECT statements.
+
+-- Question 1: What are the names of the tracks with danceability between 30% and 50%?
+
+-- Question 2: "Acousticness" is an interesting quality. Which songs have less than 1% acousticness?
+
+-- Question 3: What are the genres of the songs with 96 popularity?
+
+-- Question 4: Which pop songs were recorded under the label Island Records?
+
+-- Question 5: Retrieve the track names and release dates for songs with a Tempo between 100 and 120 BPM.
+
+-- Question 6: Are any of these songs over 5 minutes long? Provide the track and artist names.
+
+-- Question 7: Which were the songs released in April? Provide the track and artist names.
+
+-- Question 8: I want to know if any of these songs were released in the last 4 months of 2023. Provide the track and artist names, along with the date of release.
+
+-- Question 9: I can't remember the name of a song. It's by Sabrina Carpenter and contains the word "please"! Is it in this list?
+
+-- Question 10: Which of these song names contain the word "love"?
+
+-- Question 11: When a track or artist name contains an "!", I think the song would have high energy or high danceability (over 50%). Is that true?
+
+-- Question 12: Which of Billie Eillish's songs are in this Top 50 - Global playlist?
+
+-- Question 13: List the song names where the Added By field is empty.
+
+-- Question 14: Which unique record labels are featured in this list?
+
+-- Question 15: Which of the songs are recorded by multiple artists?
+
