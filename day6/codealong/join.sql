f66e1643b5a2b558ce0dd32c680fddd5a78890f7
Varsha-ACAD <varsha.v@atriauniversity.edu.in>
2024-10-08 03:08:31 +0000

    Adding CodeAlong Day6

---
+++ day6/codealong/join.sql
@@ -0,0 +1,19 @@
+-- Joins --
+-- 1a. Retrieve the names of players along with the number of games they played in 2020.
+-- 1b. Retrieve the names of players along with the names of the parks where their teams played in 2021.
+-- 2. Retrieve all player names, along with their performance records, including those players who have no performances in 2021.
+-- 3. Retrieve the names of players, their team names, and the salary they earned in 2020.
+-- 4. List the names of players who hit more than 20 home runs in a single season, along with the year and the team they played for.
+-- 5. List the first & last name of the top 5 players with most home runs across their career.
+
+
+
+-- Set Operations --
+-- UNION, INTERSECT, EXCEPT
+
+-- 1. Find the names of players who batted OR threw left-handed (use UNION).
+-- 2. Find the names of players who both batted and threw left-handed (use INTERSECT).
+-- 3. Retrieve the names of players who were born in the USA but not in California (use EXCEPT).
+-- 4. Find the names of players who played in 2020 but did not play in 2021 (use EXCEPT).
+
+-- Do we need set operations to answer these questions?
