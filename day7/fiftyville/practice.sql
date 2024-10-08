d66e77e2c7f6359d36a15bf7264da0cb8479dd70
Varsha-ACAD <varsha.v@atriauniversity.edu.in>
2024-10-08 12:01:03 +0000

    Day 7 Files

---
+++ day7/fiftyville/practice.sql
@@ -0,0 +1,12 @@
+-- Practice Questions - increasing level of complexity
+
+-- 1. List the names and phone numbers of all people.
+-- 2. List the names of all people who don't have a passport. (Use IS NOT NULL!)
+-- 3. List the flights on July 28, ordered by time.
+-- 4. Which months in 2023 had over 40 reported crimes?
+-- 5. Which street is the most prone to crime?
+-- 6. How many people have mentioned 'Sherlock Holmes' in their interviews?
+-- 7. Give details about the phone calls made by Abigail.
+-- 8. How many flights has Margaret travelled by?
+-- 9. On what days has Marget travelled by flights?
+-- 10. Which destinations has Margaret flown to?
